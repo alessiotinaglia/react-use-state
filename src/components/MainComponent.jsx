fdb530d8e308b863069fe16d3f6f289179da68a2
alessiotinaglia <alessio.tina1998@gmail.com>
2024-12-16 17:38:57 +0100

    fine secondo punto

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 
-const MainComponent = ({ languages }) => { 
+const MainComponent = ({ languages }) => {
+  const [linguaggioSelezionato, setLinguaggioSelezionato] = useState(languages[0]); 
+
+  // gestisce il click dei bottoni
+  const gestisciClickBottone = (linguaggio) => {
+    setLinguaggioSelezionato(linguaggio);
+  };
+
   return (
     <div>
-      {languages.map(element => (
-        <button key={element.id} className="btn btn-primary m-2">
+      {/* fa vedere i bottoni con html /css ecc... */}
+      {languages.map((element) => (
+        <button 
+          key={element.id} 
+          className="btn btn-primary m-2"
+          onClick={() => gestisciClickBottone(element)}
+        >
           {element.title}
         </button>
       ))}
+      {/* fa vedere la card */}
+      <div className="card mt-4">
+        <div className="card-body">
+          <h5 className="card-title">{linguaggioSelezionato.title}</h5>
+          <p className="card-text">{linguaggioSelezionato.description}</p>
+        </div>
+      </div>
     </div>
   );
 };
